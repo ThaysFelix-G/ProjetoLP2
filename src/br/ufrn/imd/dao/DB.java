5a0678401aed2b03198008535382d4293da913dd
ThaysFelix-G <71108281+ThaysFelix-G@users.noreply.github.com>
2022-07-20 23:23:39 -0300

    Adicionado métodos remover, atualizar e ler
    adicionado também o scanner e um menu

--- src/br/ufrn/imd/dao/DB.java
+++ src/br/ufrn/imd/dao/DB.java
@@ -1,75 +1,134 @@
 package br.ufrn.imd.dao;
 
 import br.ufrn.imd.controle.Usuario;
+import br.ufrn.imd.interfaces.Crud;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
-public class DB {
+public class DB implements Crud {
 
-    BufferedWriter  pw;
-    BufferedReader in;
-    File arquivo;
+    String p;
+    public DB(){}
     public DB(String caminho) throws IOException {
-        String s = "src\\br\\ufrn\\imd\\dao\\";
-        s += caminho;
-        arquivo = new File(caminho);
-        in = new BufferedReader(new FileReader(arquivo));
-        pw = new BufferedWriter(new FileWriter(arquivo,true));
-
+        p = caminho;
     }
-    private void escreverLinha(String texto) throws IOException {
+    private void escreverLinha(String texto, boolean override) throws IOException {
         try {
+            File arquivo = new File(p);
+            BufferedWriter  pw = new BufferedWriter(new FileWriter(arquivo,override));
             pw.write(texto);
             pw.close();
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
     }
-
+    @Override
     public Boolean criar(Object object) throws IOException {
         if(object instanceof Usuario){
+            File arquivo = new File(p);
             long count = Files.lines(Path.of(arquivo.getAbsolutePath())).count();
 
             String insert = (count / 4) + 1 + "\n" +((Usuario) object).toString();
-            this.escreverLinha(insert);
+            this.escreverLinha(insert, true);
         }
-        /*else
-        {
-        }*/
         return true;
     }
 
+    @Override
+    public Boolean remover(int id) throws IOException {
+        String saida ="";
+        File arquivo = new File(p);
+        BufferedReader in = new BufferedReader(new FileReader(arquivo));
 
-    public Boolean remover(int id) {
-        return null;
-    }
-
-
-    public Boolean atualizar(Object object) {
-        return null;
-    }
-
-
-    public Object ler(int id) throws IOException {
         String linha;
 
-        while ((linha = in .readLine()) != null) {
-            System.out.println("Linha: " + linha);
-            if(linha.contains(String.valueOf(id))){
-                //for (int i = 0; i < 3 ; i++) {
-                    //System.out.println((linha.lines().));
-                //}
-                System.out.println("ACHOU!!!");
-                //return false;
+        while ((linha = in.readLine()) != null) {
+            if (linha.equals(Integer.toString(id))) {
+                int count = 0;
+                while ((linha = in.readLine()) != null) {
+                    if (count ==  2) {
+                        break;
+                    }
+                    count++;
+                }
+            } else{
+                saida += linha + "\n";
             }
-
-
         }
 
+        this.escreverLinha(saida, false);
+        in.close();
         return null;
+    }
 
+    @Override
+    public Boolean atualizar(Object object) throws IOException {
+        String saida ="";
+        File arquivo = new File(p);
+        BufferedReader in = new BufferedReader(new FileReader(arquivo));
+
+        String linha;
+        Usuario u = new Usuario();
+
+        if(object instanceof Usuario){
+            u = (Usuario) object;
+        }
+        while ((linha = in.readLine()) != null) {
+            if (linha.equals(Integer.toString(u.getId()))) {
+                saida += linha + "\n";
+                int count = 0;
+                while ((linha = in.readLine()) != null) {
+                    if(count == 0){
+                        saida += u.getNome() + "\n";
+                    } else if(count == 1){
+                        saida += u.getEmail() + "\n";
+                    } else if (count ==  2){
+                        saida += u.getSenha() + "\n";
+                        break;
+                    }
+                    count++;
+                }
+            } else{
+                saida += linha + "\n";
+            }
+        }
+
+        this.escreverLinha(saida, false);
+        in.close();
+        return null;
+    }
+
+    public Object ler(int id) throws IOException {
+        File arquivo = new File(p);
+        BufferedReader in = new BufferedReader(new FileReader(arquivo));
+
+        String linha;
+        while ((linha = in.readLine()) != null) {
+            if (linha.equals(Integer.toString(id))) {
+                Usuario usuario = new Usuario();
+                usuario.setId(Integer.parseInt(linha.trim()));
+                int count = 0;
+                while ((linha = in.readLine()) != null) {
+                    if(count == 0){
+                        usuario.setNome(linha);
+                    } else if(count == 1){
+                        usuario.setEmail(linha);
+                    } else if (count ==  2){
+                        usuario.setSenha(linha);
+                        return usuario;
+                    }
+                    count++;
+                }
+            }
+
+        }
+        in.close();
+        return null;
     }
 
 }
--- src/br/ufrn/imd/visao/Main.java
+++ src/br/ufrn/imd/visao/Main.java
@@ -6,17 +6,84 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void menu() {
+        System.out.println("Escolha uma das opcoes");
+        System.out.println("1: cadastrar usuario");
+        System.out.println("2: remover usuario");
+        System.out.println("3: atualizar usuario");
+        System.out.println("4: pesquisar usuario");
+        System.out.println("5: finalizar");
+        System.out.println("");
+    }
 
-        Usuario usuario = new Usuario("src\\br\\ufrn\\imd\\dao\\usuario.txt");
-        usuario.nome = "Thays";
-        usuario.criar(usuario);
-        Usuario usuario1 = new Usuario("src\\br\\ufrn\\imd\\dao\\usuario.txt");
-        usuario1.nome = "Thays";
-        usuario1.criar(usuario1);
-        usuario.ler(2);
+    public static void main(String[] args) throws IOException {
+        Scanner leia = new Scanner(System.in);
+        while (true) {
+            menu();
+            int opcao = leia.nextInt();
+
+            switch (opcao) {
+                case 1: {
+                    Usuario user = new Usuario("src\\br\\ufrn\\imd\\dao\\usuario.txt");
+                    System.out.print("Digite o nome do usuario: ");
+                    user.setNome(String.valueOf(leia.next().replaceAll("[ ]+", " ").split(" ")));
+                    System.out.print("Digite o email do usuario: ");
+                    user.setEmail(leia.next().replaceAll(" ", "\n"));
+                    System.out.print("Digite a senha do usuario: ");
+                    user.setSenha(leia.next());
+                    user.criar(user);
+                    System.out.println("Usuario criado com sucesso");
+                    continue;
+                }
+                case 2: {
+                    System.out.print("Informe o id do usuario que deseja remover: ");
+                    Usuario user = new Usuario("src\\br\\ufrn\\imd\\dao\\usuario.txt");
+                    int id = leia.nextInt();
+                    Object u = new Usuario();
+                    u = user.ler(id);
+                    user.remover(id);
+                    System.out.println("usuario removido!!!!");
+                    System.out.println(u.toString());
+                    continue;
+
+                }
+                case 3: {
+                    Usuario user = new Usuario("src\\br\\ufrn\\imd\\dao\\usuario.txt");
+                    System.out.print("Digite o id do usuario: ");
+                    int id = leia.nextInt();
+                    user.setId(id);
+                    System.out.print("Digite o novo nome do usuario: ");
+                    user.setNome(leia.next());
+                    System.out.print("Digite o novo email do usuario: ");
+                    user.setEmail(leia.next());
+                    System.out.print("Digite a nova senha do usuario: ");
+                    user.setSenha(leia.next());
+                    user.atualizar(user);
+                    System.out.println("Usuario atualizado com sucesso!!!!");
+                    continue;
+                }
+                case 4: {
+                    Usuario user = new Usuario("src\\br\\ufrn\\imd\\dao\\usuario.txt");
+                    System.out.print("Digite o id do usuario para pesquisa: ");
+                    int id = leia.nextInt();
+                    user = (Usuario) user.ler(id);
+                    System.out.println(user.toString());
+                    continue;
+                }
+                case 5: {
+                    System.out.println("Saindo...");
+                    System.exit(0);
+                }
+                default: {
+                    System.out.println("Opcao invalida!!!!!");
+                    continue;
+                }
+            }
+
+        }
 
     }
 }
